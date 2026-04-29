ab292768f9e2bd07b27715757c9a0a2572ef3eb0
Guilherme <ggomesns@gmail.com>
2026-04-29 11:37:15 -0300

    Adicionado arquivos frontend e CORS middleware configurado para API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.routes.financeiro_routes import router as financeiro_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="FinTrack")
+app = FastAPI(title="API Controle Financeiro")
+
+#liberar o cors para tetes e desenvolvimento
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+)
+
+
 
 app.include_router(financeiro_router)
 
